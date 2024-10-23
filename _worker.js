37357b27595954e86ca337f12016c5082fe31256
win877969 <xpandhita@gmail.com>
2024-10-23 19:06:45 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -510,7 +510,31 @@ vless://d2eed70a-4102-42b0-8b40-279e6d901a02@cf-prem.bmkg.xyz:80?path=/vl=35.219
 
 
 
+<div ID="Show" VALUE="Click to show" ONCLICK=" document.getElementById('Show').style.display='none'; document.getElementById('Hide').style.display='block'; document.getElementById('SectionName').style.display='block'" >SHOW</DIV> <div ID="Hide" VALUE="Click to hide" ONCLICK=" document.getElementById('Show').style.display='block'; document.getElementById('Hide').style.display='none'; document.getElementById('SectionName').style.display='none'" hidden>HIDE</DIV> <div ID="SectionName" STYLE="display:none" name="m" rows="5" cols="50" on_click="this.value='Hello\nHow R U?'" >The 
+Text
+The 
+Next Line<textarea name="m" rows="5" cols="50" on_click="this.value='Hello\nHow R U?'" id="myInput" >The 
+Text
+The 
+Next 
+Line </textarea>
+<div onclick="myFunction()">Copy text</button></DIV>
+<script>
+function myFunction() {
+  // Get the text field
+  var copyText = document.getElementById("myInput");
 
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+  
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
+</script>
 
 
 
